5e3605b9ecd14c538c5d649cda43d895bcd3d79c
bili9blood <3501202786@qq.com>
2023-12-28 21:10:23 +0800

    feat:basic login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,24 @@
+import { createSignal } from "solid-js";
 import PeopleSvg from "../assets/people.svg";
+import TextInput from "../components/TextInput";
+
+const [username, setUsername] = createSignal("");
+const [password, setPassword] = createSignal("");
+
+function LoginForm() {
+  return (
+    <div class="flex flex-col gap-2 items-center justify-items-center w-[60%]">
+      <p class="text-2xl">登录</p>
+      <br />
+      <TextInput name="用户名" type="text" valueRef={[username, setUsername]} />
+      <TextInput
+        name="密码"
+        type="password"
+        valueRef={[password, setPassword]}
+      />
+    </div>
+  );
+}
 
 export default function () {
   return (
@@ -8,6 +28,7 @@ export default function () {
         alt="people"
         class="w-[40%] h-full p-5 bg-sky-200 rounded-l-lg pointer-events-none select-none"
       />
+      <LoginForm />
     </div>
   );
 }
