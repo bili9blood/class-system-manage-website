e028b0ad8ac290aca6670ac02a66e7686f5e36d0
bili9blood <3501202786@qq.com>
2023-12-29 19:30:56 +0800

    feat: 404 page can jump to index page

--- src/pages/[...all].tsx
+++ src/pages/[...all].tsx
@@ -4,7 +4,7 @@ export default function () {
   return (
     <div class="flex flex-col items-center gap-4 py-4">
       <p class="mx-16 text-8xl font-bold">404</p>
-      <A class="btn" href="/login">
+      <A class="btn" href="/">
         主页
       </A>
     </div>
