6f4a522d9719b15895cf0415a8edd0dd86cb8f1b
bili9blood <3501202786@qq.com>
2023-12-29 19:51:26 +0800

    feat: mobile login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,8 +7,8 @@ const [password, setPassword] = createSignal("");
 
 function LoginForm() {
   return (
-    <div class="flex w-[60%] flex-col items-center justify-items-center gap-2">
-      <p class="text-2xl">登录</p>
+    <div class="flex w-full flex-col items-center justify-items-center gap-2 md:w-[60%]">
+      <p class="text-2xl">管理后台登录</p>
       <br />
       <TextInput name="用户名" type="text" valueRef={[username, setUsername]} />
       <TextInput
@@ -22,11 +22,11 @@ function LoginForm() {
 
 export default function () {
   return (
-    <div class="flex h-[50vh] min-h-[20rem] w-[50vw] items-center">
+    <div class="m-auto flex h-[50vh] items-center overflow-hidden md:min-h-[20rem] md:w-[50vw] md:min-w-[35rem]">
       <img
         src={PeopleSvg}
         alt="people"
-        class="pointer-events-none h-full w-[40%] select-none rounded-l-lg bg-sky-200 p-5"
+        class="pointer-events-none hidden h-full w-[40%] select-none rounded-l-lg bg-sky-200 p-5 md:block"
       />
       <LoginForm />
     </div>
